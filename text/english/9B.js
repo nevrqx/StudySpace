fc9f15e02bc4d915eec5c1993b38e220be9074db
nevrqx <nevrqx@gmail.com>
2026-01-03 01:15:27 +0300

    Add files via upload

---
+++ text/english/9B.js
@@ -0,0 +1,16 @@
+window.text9B = {
+    id: '9B',
+    topic: 'Literature',
+    titleEn: "Carol Ann Duffy - We Remember Your Childhood Well",
+    titleRu: "Кэрол Энн Даффи - Мы хорошо помним твоё детство",
+    contentEn: [
+        "Dame Carol Ann Duffy was born to a working-class Catholic family in Glasgow in 1955. When she was 14 she decided she wanted to be a poet, and two years later Outposts, a leading poetry magazine, published some of her poems. After obtaining a degree in philosophy, she worked as a scriptwriter for television, a freelance writer and a poetry editor. She is also highly regarded as a playwright.",
+        "In 1983 she won the National Poetry Competition with Whoever She Was, and then the Scottish Arts Council Award for her first full collection of poetry. Since then she has had five further collections published. Many of her poems are in the form of a dramatic monologue, which is rather like a speech from a play: a character speaks, giving clues to the sort of person they are, who they are speaking to, and the situation.",
+        "In 1994 her poetry was placed on the school syllabus for England and Wales, and her work continues to be widely studied in British schools. She was appointed Poet Laureate in 2009, the first woman to hold the position. She was named a Dame Commander of the British Empire in 2015."
+    ],
+    contentRu: [
+        "Кэрол Энн Даффи родилась в рабочей католической семье в Глазго в 1955 году. В возрасте 14 лет она решила, что хочет стать поэтом, и два года спустя в ведущем поэтическом журнале Outposts были опубликованы некоторые из её стихотворений. Получив степень по философии, она работала сценаристом на телевидении, писателем-фрилансером и редактором поэзии. Она также высоко ценится как драматург.",
+        "В 1983 году она выиграла Национальный поэтический конкурс со стихотворением \"Кем бы она ни была\", а затем получила премию Совета по искусству Шотландии за свой первый полный поэтический сборник. С тех пор у нее вышло еще пять сборников. Многие из ее стихотворений написаны в форме драматического монолога, который чем-то напоминает речь из пьесы: персонаж говорит, давая подсказки о том, что он за человек, к кому он обращается и какова ситуация.",
+        "В 1994 году ее поэзия была включена в школьную программу Англии и Уэльса, и ее работы продолжают широко изучаться в британских школах. В 2009 году она была назначена поэтом-лауреатом, став первой женщиной, занявшей этот пост. В 2015 году она была удостоена звания дамы-командора ордена Британской империи."
+    ]
+};
